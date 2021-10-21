b0d51f98fd461e3bc0b8b57d7b7a287a67ccc8e7
rohanghosh <rohangho@live.com>
2021-10-22 00:40:29 +0530

    patch 3

--- app/src/main/java/com/example/pathdemo/MainActivity.kt
+++ app/src/main/java/com/example/pathdemo/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.e("MainActivity", "patch file" );
+        Log.e("MainActivity", "patch file 2" );
     }
 }
